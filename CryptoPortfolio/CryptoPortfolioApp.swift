07edb4255f9e480db0debeeb380e7051ddbd9761
Caio Castro <caio.castro3909@gmail.com>
2022-11-28 15:16:24 +0100

    Initial Commit

---
+++ CryptoPortfolio/CryptoPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPortfolioApp.swift
+//  CryptoPortfolio
+//
+//  Created by Caio Castro on 28/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
